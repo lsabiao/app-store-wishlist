23d6c29c65318c4a7ab902d9f26504e8cd4d1231
Lucas Sabião <lucas.sabiao@hotmail.com>
2023-02-16 13:21:36 -0300

    finishing the front-end

--- src/server.py
+++ src/server.py
@@ -1,5 +1,25 @@
-from bottle import route, run, template
+from bottle import route, run, template, static_file
 import updater
+
+# Static Routes
+@route("/static/css/<filepath:re:.*\.css>")
+def css(filepath):
+    return static_file(filepath, root="static/css")
+
+@route("/static/font/<filepath:re:.*\.(eot|otf|svg|ttf|woff|woff2?)>")
+def font(filepath):
+    return static_file(filepath, root="static/font")
+
+@route("/static/img/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
+def img(filepath):
+    return static_file(filepath, root="static/img")
+
+@route("/static/js/<filepath:re:.*\.js>")
+def js(filepath):
+    return static_file(filepath, root="static/js")
+
+
+
 
 @route('/')
 def index():
